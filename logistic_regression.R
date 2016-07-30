20d6bf486795ba593c3069421123633001fb6b22
janeparkhere <jpark15@alumni.nd.edu>
2016-07-30 18:25:39 -0400

    Add files via upload
    logistic regression exercise(s)

---
+++ logistic_regression.R
@@ -0,0 +1,175 @@
+## Regression with binary outcomes
+## ═════════════════════════════════
+
+## Logistic regression
+## ───────────────────────
+
+##   This far we have used the `lm' function to fit our regression models.
+##   `lm' is great, but limited–in particular it only fits models for
+##   continuous dependent variables. For categorical dependent variables we
+##   can use the `glm()' function.
+
+##   For these models we will use a different dataset, drawn from the
+##   National Health Interview Survey. From the [CDC website]:
+
+##         The National Health Interview Survey (NHIS) has monitored
+##         the health of the nation since 1957. NHIS data on a broad
+##         range of health topics are collected through personal
+##         household interviews. For over 50 years, the U.S. Census
+##         Bureau has been the data collection agent for the National
+##         Health Interview Survey. Survey results have been
+##         instrumental in providing data to track health status,
+##         health care access, and progress toward achieving national
+##         health objectives.
+
+##   Load the National Health Interview Survey data:
+getwd()
+setwd("C:/Users/jpark.FACS/Desktop/jptrain/logistic_regression")
+NH11 <- readRDS("dataSets/NatHealth2011.rds")
+head(NH11)
+
+
+
+############################### tells you what the heck each of these variables means!!
+labs <- attributes(NH11)$labels
+labs
+
+##   [CDC website] http://www.cdc.gov/nchs/nhis.htm
+
+## Logistic regression example
+## ───────────────────────────────
+
+##   Let's predict the probability of being diagnosed with hypertension
+##   based on age, sex, sleep, and bmi
+str(NH11$hypev) # check stucture of hypev
+levels(NH11$hypev) # check levels of hypev
+# collapse all missing values to NA
+NH11$hypev <- factor(NH11$hypev, levels=c("2 No", "1 Yes"))
+# run our regression model
+hyp.out <- glm(hypev~age_p+sex+sleep+bmi, data=NH11, family="binomial")
+summary(hyp.out)
+coef(summary(hyp.out))
+
+## Logistic regression coefficients
+## ────────────────────────────────────
+
+##   Generalized linear models use link functions, so raw coefficients are
+##   difficult to interpret. For example, the age coefficient of .06 in the
+##   previous model tells us that for every one unit increase in age, the
+##   log odds of hypertension diagnosis increases by 0.06. Since most of us
+##   are not used to thinking in log odds this is not too helpful!
+
+##   One solution is to transform the coefficients to make them easier to
+##   interpret
+
+hyp.out.tab <- coef(summary(hyp.out))
+hyp.out.tab[, "Estimate"] <- exp(coef(hyp.out))
+hyp.out.tab
+
+## Generating predicted values
+## ───────────────────────────────
+
+##   In addition to transforming the log-odds produced by `glm' to odds, we
+##   can use the `predict()' function to make direct statements about the
+##   predictors in our model. For example, we can ask "How much more likely
+##   is a 63 year old female to have hypertension compared to a 33 year old
+##   female?".
+
+# Create a dataset with predictors set at desired levels
+?with
+predDat <- with(NH11,
+                expand.grid(age_p = c(33, 63),
+                            sex = "2 Female",
+                            bmi = mean(bmi, na.rm = TRUE),
+                            sleep = mean(sleep, na.rm = TRUE)))
+predDat
+# predict hypertension at those levels
+cbind(predDat, predict(hyp.out, type = "response",
+                       se.fit = TRUE, interval="confidence",
+                       newdata = predDat))
+
+##   This tells us that a 33 year old female has a 13% probability of
+##   having been diagnosed with hypertension, while and 63 year old female
+##   has a 48% probability of having been diagnosed.
+
+## Packages for  computing and graphing predicted values
+## ─────────────────────────────────────────────────────────
+
+##   Instead of doing all this ourselves, we can use the effects package to
+##   compute quantities of interest for us (cf. the Zelig package).
+install.packages("effects")
+library(effects)
+plot(allEffects(hyp.out))
+
+## Exercise: logistic regression
+## ───────────────────────────────────
+
+##   Use the NH11 data set that we loaded earlier.
+head(NH11)
+str(NH11)
+##   1. Use glm to conduct a logistic regression to predict ever worked
+##      (everwrk) using age (age_p) and marital status (r_maritl).
+
+library(caTools)
+set.seed(1000)
+split <- sample.split(NH11$everwrk, SplitRatio=0.50)
+train <- subset(NH11, split == TRUE)
+test <- subset(NH11, split == FALSE)
+############################### WRONG
+############################### copy-pasted from example above
+str(NH11$everwrk) # check stucture of everwrk
+levels(NH11$everwrk) # check levels of everwrk
+# collapse all missing values to NA
+NH11$everwrk <- factor(NH11$everwrk, levels=c("2 No", "1 Yes"))
+
+table(train$everwrk)
+############################## benchmark is frequency of most frequent outcome
+6076/(6076+944+8+4)
+############################## 0.8640501 is accuracy benchmark;
+############################## our log reg model should have better accuracy than 86.4%
+
+
+nhLog <- glm(everwrk~age_p+r_maritl, data=train, family="binomial")
+summary(nhLog)
+########## training set nhLog's AIC = 5216.1
+
+predictTrain <- predict(nhLog, type="response")
+predictTrain
+table(train$everwrk, predictTrain > 0.5)
+
+predictTest <- predict(nhLog, type="response", newdata=test)
+table(test$everwrk, predictTest > 0.5)
+
+
+
+############################# RIGHT
+nh11_relevant <- subset(NH11, select = c("everwrk", "age_p", "r_maritl"))
+summary(nh11_relevant)
+
+NH11 <- transform(NH11, everwrk = factor(everwrk, levels = c("1 Yes", "2 No")), r_maritl = droplevels(r_maritl))
+
+nh11_Log <- glm(everwrk~age_p+r_maritl, data=NH11, family="binomial")
+
+summary(nh11_Log)
+
+##   2. Predict the probability of working for each level of marital
+##      status.
+
+##################### WRONG
+summary(NH11$r_maritl)
+summary(NH11)
+plot(allEffects(nhLog))
+
+
+#################### RIGHT
+library(effects)
+data.frame(Effect("r_maritl", nh11_Log))
+
+
+
+
+
+##   Note that the data is not perfectly clean and ready to be modeled. You
+##   will need to clean up at least some of the variables before fitting
+##   the model.
+
